edbb8af9662967183e3fb8e6ae3d56fa5108b3f9
Xeeshan <xeeshann.ahsan@gmail.com>
2026-09-12 23:05:40 +0600

    supabase

--- database.py
+++ database.py
@@ -3,10 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./library.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./library.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.atzvbqmtvzsorjfmzbgo:9Aug2026project@aws-0-ap-south-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
