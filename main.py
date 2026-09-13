f92d1a284f120e9751db807df63491825865f6c3
Xeeshan <xeeshann.ahsan@gmail.com>
2026-09-13 11:45:23 +0600

    all book api set

--- main.py
+++ main.py
@@ -31,10 +31,6 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @app.get("/books/all")
 def get_all_books(user: user_dependency, db: db_dependency):
-
-    if user is None:
-        raise HTTPException(status_code=401, detail="Failed to authenticate user")
-
     books = db.query(Books).all()
     return books
 
